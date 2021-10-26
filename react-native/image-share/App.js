fcbff6050770e0f7244146e3057b48ad9c612936
Svetlana Kachkova <sv.kachkova@gmail.com>
2021-10-26 09:35:47 +0300

    add react native

---
+++ react-native/image-share/App.js
@@ -0,0 +1,99 @@
+import React, { useState } from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import logo from './assets/logo.png';
+import * as ImagePicker from 'expo-image-picker';
+import * as Sharing from 'expo-sharing'; 
+
+export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const openImagePickerAsync = async () => {
+    const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert('Permission to access camera roll is required!');
+      return;
+    }
+
+    const pickerResult = await ImagePicker.launchImageLibraryAsync();
+
+    if (pickerResult.cancelled === true) return;
+
+    setSelectedImage({ localUri: pickerResult.uri });
+  };
+
+  const openShareDialogAsync = async () => {
+    if (!(await Sharing.isAvailableAsync())) {
+      alert(`Uh oh, sharing isn't available on your platform`);
+      return;
+    }
+
+    await Sharing.shareAsync(selectedImage.localUri);
+  }; 
+
+  if (selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image source={{ uri: selectedImage.localUri }} style={styles.thumbnail} />
+        <Button handlePress={openShareDialogAsync}>Share this photo</Button>
+        <Button handlePress={() => setSelectedImage(null)}>Pick new photo</Button>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <Image source={logo} style={styles.logo} />
+      <Text style={styles.instructions}>
+        To share a photo from your phone with a friend, just press the button below!
+      </Text>
+      <Button handlePress={openImagePickerAsync}>Pick a photo</Button>
+    </View>
+  );
+}
+
+function Button(props) {
+  return (
+    <View>
+      <TouchableOpacity onPress={props.handlePress} style={styles.button}>
+        <Text style={styles.buttonText}>{props.children}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 10,
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 20,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+  },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: 'contain',
+    marginBottom: 10,
+  },
+});
